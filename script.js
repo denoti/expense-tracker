63650c710559e82a19bfe4f882744775cf37696c
Dennis Otieno Omondi <otidennis98@gmail.com>
2023-09-05 21:13:54 +0300

    updates

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let { log } = console;
 const addBtn = document.querySelector('.add');
 const submitBtn = document.querySelector('.submit');
 const itemContainer = document.querySelector('.item-container');
+const expensesValues = document.querySelector('.expenses-values');
+const inputExpenses = document.querySelector('.input-expenses');
+const homepageBtn = document.querySelector('.home-expense');
+const viewExpenseBtn = document.querySelector('.view-expenses');
+log(inputExpenses);
 
 function createBudget() {
   let valueContainer = document.createElement('div');
@@ -40,4 +45,13 @@ addBtn.onclick = () => {
   log(itemContainer.children.length);
 };
 
-log(itemContainer.children.length);
+log(viewExpenseBtn);
+
+viewExpenseBtn.onclick = () => {
+  inputExpenses.classList.add('hide');
+  expensesValues.classList.remove('hide');
+};
+homepageBtn.onclick = () => {
+  inputExpenses.classList.remove('hide');
+  expensesValues.classList.add('hide');
+};
